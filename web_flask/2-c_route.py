33e47cb14429c4e6dabc224b71a815dd1283b926
fatima zahra lamrani <fatimazahralamrani26@gmail.com>
2023-11-25 02:23:12 -0800

    tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 '''starts a Flask web application'''
-
 from flask import Flask
 
 exu = Flask(__name__)
 
+@exu.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@exu.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
 
 @exu.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
+def c(text):
     return 'C ' + text.replace('_', ' ')
 
-
 if __name__ == '__main__':
-    exu.run(host='0.0.0.0', port='5000')    
+    exu.run(host='0.0.0.0', port=5000)
